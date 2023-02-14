ce0cfd295e700d2a74e95238711195895da5a77d
AimerAttack <wcpwcp9@gmail.com>
2023-02-14 16:45:56 +0800

    UI、逻辑分离的模板文件

---
+++ Assets/Editor/EditorUIMaker/Template/EditorLogic.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace EditorUIMaker.Template
+{
+    public class EditorLogic
+    {
+        public bool Toggle1;
+        public int IntSlider1;
+        
+        public void OnIntSlider1Changed(int val)
+        {
+            
+        }
+
+        public void OnToggle1Changed(bool val)
+        {
+            
+        }
+        
+        public void ClickButton1()
+        {
+            
+        }
+    }
+}
