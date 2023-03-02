94c22922cd3c8c182eb0d9a216e806f23971637d
AimerAttack <wcpwcp9@gmail.com>
2023-03-02 17:01:29 +0800

    自定义插件逻辑

--- Assets/Editor/EditorUIMaker/ScriptableObject/EUM_Setting.cs
+++ Assets/Editor/EditorUIMaker/ScriptableObject/EUM_Setting.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using EditorUIMaker.Widgets;
 using Sirenix.OdinInspector;
 using UnityEditor;
 using UnityEngine;
@@ -8,5 +9,7 @@ namespace EditorUIMaker
     public class EUM_Setting : SerializedScriptableObject
     {
         public List<string> AdditionNamespaces;
+        public List<EUM_Widget> Widgets;
+        public List<EUM_Container> Containers;
     }
 }
